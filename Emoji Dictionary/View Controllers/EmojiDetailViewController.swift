9844fb7b49693bc4bd0b20edb253a6ccaf4c226b
Иван Деркачев <longrail@yandex.ru>
2019-05-09 17:58:19 +0300

    Add keyboards dismiss by tap

--- Emoji Dictionary/View Controllers/EmojiDetailViewController.swift
+++ Emoji Dictionary/View Controllers/EmojiDetailViewController.swift
@@ -15,6 +15,7 @@ class EmojiDetailViewController: UIViewController {
     @IBOutlet weak var descriptionField: UITextField!
     @IBOutlet weak var usageField: UITextField!
     @IBOutlet weak var scrollViewBottomLayout: NSLayoutConstraint!
+    var keyboardDismissTapGesture: UIGestureRecognizer?
     
     var emoji = Emoji()
     
@@ -22,10 +23,16 @@ class EmojiDetailViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         updateUI()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(EmojiDetailViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(EmojiDetailViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        removeKeyboardNotifications()
+        super.viewWillDisappear(animated)
     }
     
     func areFieldsReady() -> Bool {
@@ -82,14 +89,86 @@ extension EmojiDetailViewController: UITextFieldDelegate {
 
 extension EmojiDetailViewController {
     
-    @objc func keyboardWillShow(notification: NSNotification) {
+
+    func setupKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             scrollViewBottomLayout.constant = keyboardSize.height
+            addTapRecognizer()
         }
     }
     
-    @objc func keyboardWillHide(notification: NSNotification) {
-            scrollViewBottomLayout.constant = 0
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        scrollViewBottomLayout.constant = 0
+        removeTapRecognizer()
     }
+    
+    //dismiss keyboard by tap
+    func addTapRecognizer() {
+        if keyboardDismissTapGesture == nil {
+            keyboardDismissTapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+            keyboardDismissTapGesture?.cancelsTouchesInView = false
+            self.view.addGestureRecognizer(keyboardDismissTapGesture!)
+        }
+    }
+    
+    func removeTapRecognizer() {
+        if keyboardDismissTapGesture != nil {
+            self.view.removeGestureRecognizer(keyboardDismissTapGesture!)
+            keyboardDismissTapGesture = nil
+        }
+    }
+    
+    @objc func dismissKeyboard(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    //    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    //        super.viewWillTransition(to: size, with: coordinator)
+    //        let a = self.view.firstResponder!.frame.height
+    //        let b = self.view.firstResponder!.frame.width
+    //        let f = scrollViewBottomLayout.constant
+    //        scrollViewBottomLayout.constant = self.view.firstResponder!.frame.height
+    //    }
+    
+    //    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+    //    super.viewWillTransition(to: size, with: coordinator)
+    //
+//            if let currentTextField = self.view.firstResponder as? UITextField {
+//
+//                self.view.endEditing(true)
+//
+//                currentTextField.becomeFirstResponder()
+    //
+    //        }
+    //
+    //    }
+    //}
+    //
+    //
+    //
+//    extension UIView {
+//        var firstResponder: UIView? {
+//            guard !isFirstResponder else { return self }
+//
+//            for subview in subviews {
+//                if let firstResponder = subview.firstResponder {
+//                    return firstResponder
+//                }
+//            }
+//
+//            return nil
+//        }
+//    }
 
+    
 }
+
